fee1b06dfaabdacce13f74c19c6f3fafd4fbf1fc
birkin <birkin.diana@gmail.com>
2016-11-07 09:58:49 -0500

    adds logging.

--- get_subfolder_ids.py
+++ get_subfolder_ids.py
@@ -9,17 +9,24 @@ from __future__ import unicode_literals
     - python ./get_subfolder_ids.py
 """
 
-import json, pprint, os
+import json, logging, pprint, os
 import requests
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S' )
+log = logging.getLogger(__name__)
+log.info( 'script started' )
 
 FOLDER_API_URL = unicode( os.environ['FLDR_IDS__FOLDER_API_URL'] )
 FOLDER_API_AUTH_NAME = unicode( os.environ['FLDR_IDS__FOLDER_API_AUTH_NAME'] )
 FOLDER_API_AUTH_KEY = unicode( os.environ['FLDR_IDS__FOLDER_API_AUTH_KEY'] )
-TARGET_FOLDER_LST = json.loads( os.environ['FLDR_IDS__TARGET_FOLDER_INFO'] )  # format: [ {'folder_a': 1}, {'folder_b': 2} ]
+TARGET_FOLDER_LST = json.loads( os.environ['FLDR_IDS__PARENT_FOLDER_INFO'] )  # format: [ {'folder_a': 1}, {'folder_b': 2} ]
 
 
 def runCode( target_folder_dct ):
-    print 'starting runCode()'
+    log.debug( 'starting runCode()' )
     ## get data
     ( target_name, target_id ) = ( target_folder_dct.items()[0][0], target_folder_dct.items()[0][1] )  # single item dct
     payload = { 'folder_id': target_id }
@@ -31,12 +38,12 @@ def runCode( target_folder_dct ):
     returned_folder_name = jdict['name']
     returned_folder_id = jdict['id']
     if not returned_folder_name == target_name:
-        print 'ERROR: expected folder name ```{expected}```; got folder name ```{received}```'.format( expected=target_name, received=returned_folder_name )
+        log.error( 'ERROR: expected folder name ```{expected}```; got folder name ```{received}```'.format(expected=target_name, received=returned_folder_name) )
         return
     if not returned_folder_id == target_id:
-        print 'ERROR: expected folder id ```{expected}```; got folder name ```{received}```'.format( expected=target_id, received=returned_folder_id )
+        log.error( 'ERROR: expected folder id ```{expected}```; got folder name ```{received}```'.format(expected=target_id, received=returned_folder_id) )
         return
-    print 'name and id ok'
+    log.debug( 'name and id ok' )
     ## build output
     output = {
         'target_folder': { 'name': returned_folder_name, 'id': returned_folder_id },
@@ -45,11 +52,13 @@ def runCode( target_folder_dct ):
     for child_folder in jdict['child_folders']:
         subfolder = ( child_folder['name'], child_folder['id'] )
         output['target_subfolders'].append( subfolder )
-    # output['subfolders'].sort()
-    print 'output, ```{}```'.format( pprint.pformat(output) )
+    log.debug( 'output, ```{}```'.format(pprint.pformat(output)) )
+    # print 'output, ```{}```'.format( pprint.pformat(output) )
 
 
 if __name__ == "__main__":
+    log.debug( 'TARGET_FOLDER_LST, ```{}```'.format(pprint.pformat(TARGET_FOLDER_LST)) )
     for target_folder_dct in TARGET_FOLDER_LST:
         runCode( target_folder_dct )
-    print '\n-- END OF SCRIPT --'
+    log.debug( '-- END OF SCRIPT --' )
+    # print '\n-- END OF SCRIPT --'
